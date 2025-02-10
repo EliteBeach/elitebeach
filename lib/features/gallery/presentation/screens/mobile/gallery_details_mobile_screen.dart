6c2457ae91a31028457b7354f7679e90d3099006
EliteBeach <alfayrouzbeach@gmail.com>
2025-02-10 14:47:46 +0200

    test images

--- lib/features/gallery/presentation/screens/mobile/gallery_details_mobile_screen.dart
+++ lib/features/gallery/presentation/screens/mobile/gallery_details_mobile_screen.dart
@@ -8,9 +8,17 @@ class GalleryDetailsMobileScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: ListView.builder(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
           itemCount: imagesListPath.length,
           itemBuilder: (context, index) {
-            return InteractiveViewer(child: Image.asset(imagesListPath[index]));
+            return InteractiveViewer(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Image.asset(
+                  imagesListPath[index],
+                ),
+              ),
+            );
           }),
     );
   }
